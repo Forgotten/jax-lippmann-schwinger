ad6dfedf1615f14346c7fd0504f82a21dda04dc5
Forgotten <lzepeda@math.mit.edu>
2021-11-14 00:56:00 -0600

    feature: not-working, gradient for l2 loss handcrafted

--- jax_ls/near_field.py
+++ jax_ls/near_field.py
@@ -110,6 +110,11 @@ def near_field_map(params: NearFieldParams,
     return near_field
 
 
+########################################################################################
+########################################################################################
+# implementing the near field from vector to vector and using the born approximation
+
+
 @partial(custom_jvp, nondiff_argnums=(0,))
 def near_field_map_vect(params: NearFieldParams, 
                       nu_vect: jnp.ndarray) -> jnp.ndarray:
@@ -132,6 +137,7 @@ def near_field_map_vect(params: NearFieldParams,
 
     nm, n_angles = params.G_sample.shape
 
+    # TODO: fix this! it is just a matrix-vector multiplication
     near_field = jnp.sum( Sigma.T.reshape((n_angles, nm, 1))\
                          *params.G_sample.reshape((1, nm, n_angles)),
                          axis=1)*params.hx*params.hy
@@ -199,3 +205,294 @@ def near_field_map_vect_jvp(params, primals, tangents):
     return near_field.reshape((-1,)), delta_near_field.reshape((-1,))
 
 
+########################################################################################
+########################################################################################
+
+## adding a near field with custom J^*
+# we implemented the application of the gradient using the born approximation
+
+@partial(custom_vjp, nondiff_argnums=(0,))
+def near_field_map_vect_v2(params: NearFieldParams, 
+                        nu_vect: jnp.ndarray) -> jnp.ndarray:
+    """function to comput the near field in a circle of radious 1, 
+    in this case we use the ls_solver_batched_sigma, which already has 
+    a custom vjp """
+
+    Rhs = -(params.ls_params.omega**2)\
+           *nu_vect.reshape((-1,1))*params.G_sample
+
+    # defining the batched transformations
+    solver_batched = jit(vmap(jit(partial(ls_solver_batched_sigma,
+                                          params.ls_params,
+                                          nu_vect)),
+                              in_axes=1, 
+                              out_axes=1))
+
+    # solve for all the rhs in a vectorized fashion
+    Sigma = solver_batched(Rhs)
+
+    nm, n_angles = params.G_sample.shape
+
+    near_field = jnp.sum( Sigma.T.reshape((n_angles, nm, 1))\
+                         *params.G_sample.reshape((1, nm, n_angles)),
+                         axis=1)*params.hx*params.hy
+
+    return near_field.reshape((-1,))
+
+
+@jit
+def near_field_map_vect_v2_fwd(params: NearFieldParams, 
+                               nu_vect: jnp.ndarray):
+    
+    Rhs = -(params.ls_params.omega**2)\
+           *nu_vect.reshape((-1,1))*params.G_sample
+
+    # defining the batched transformations
+    solver_batched = jit(vmap(jit(partial(ls_solver_batched_sigma,
+                                          params.ls_params,
+                                          nu_vect)),
+                              in_axes=1, 
+                              out_axes=1))
+
+    # solve for all the rhs in a vectorized fashion
+    Sigma = solver_batched(Rhs)
+
+    nm, n_angles = params.G_sample.shape
+
+    near_field = jnp.sum( Sigma.T.reshape((n_angles, nm, 1))\
+                         *params.G_sample.reshape((1, nm, n_angles)),
+                         axis=1)*params.hx*params.hy
+
+    return near_field.reshape((-1,)), (Sigma, nu_vect)
+
+
+@jit
+def near_field_map_vect_v2_bwd(params, fwd_res, res):
+    """ Function to compute adjoint of the near field operator
+    """
+
+    Sigma, nu_vect = fwd_res
+
+    # defining the batched for the adjoint problem
+    solver_adj_batched = jit(vmap(jit(partial(ls_solver_batched_adj,
+                                          params.ls_params,
+                                          nu_vect)),
+                              in_axes=1, 
+                              out_axes=1))
+
+    conj_green_batched = jit(vmap(jit(partial(apply_conj_green_function,
+                                         params.ls_params)),
+                             in_axes=1,
+                             out_axes=1))
+
+    green_batched = jit(vmap(jit(partial(apply_green_function,
+                                         params.ls_params)),
+                             in_axes=1,
+                             out_axes=1))
+
+
+    # computing the scattered field
+    U_s = green_batched(Sigma)
+
+    # computing the total field
+    U_total = U_s + params.G_sample
+
+    nm, n_angles = params.G_sample.shape
+
+    # assembling the adjoint RHS for the integral equation
+    Rhs_adjoint = (params.ls_params.omega**2)*jnp.conj(params.G_sample)@res.reshape((n_angles,n_angles))
+
+    # solve for all the rhs in a vectorized fashion
+    Sigma_adj = solver_adj_batched(Rhs_adjoint)
+
+    # computing the scattered field
+    U_adj = conj_green_batched(Sigma_adj)
+
+    return (jnp.sum(jnp.conj(U_total)*U_adj, axis = 1),)
+
+
+# we define the application of the adjoit
+near_field_map_vect_v2.defvjp(near_field_map_vect_v2_fwd, near_field_map_vect_v2_bwd)
+
+########################################################################################
+########################################################################################
+### We implement the L2 loss function
+
+## implementing the L2 loss
+@partial(custom_vjp, nondiff_argnums=(0,1))
+def near_field_l2_loss(params: NearFieldParams, 
+            u_data: jnp.ndarray,
+            nu_vect: jnp.ndarray) -> jnp.ndarray:
+    """function to comput the near field in a circle of radious 1, 
+    in this case we use the ls_solver_batched_sigma, which already has 
+    a custom jvp implemented """
+
+    Rhs = -(params.ls_params.omega**2)\
+           *nu_vect.reshape((-1,1))*params.G_sample
+
+    # defining the batched transformations
+    solver_batched = jit(vmap(jit(partial(ls_solver_batched_sigma,
+                                          params.ls_params,
+                                          nu_vect)),
+                              in_axes=1, 
+                              out_axes=1))
+
+    # solve for all the rhs in a vectorized fashion
+    Sigma = solver_batched(Rhs)
+
+    nm, n_angles = params.G_sample.shape
+
+    near_field = jnp.sum( Sigma.T.reshape((n_angles, nm, 1))\
+                         *params.G_sample.reshape((1, nm, n_angles)),
+                         axis=1)*params.hx*params.hy
+
+    return 0.5*jnp.sum(jnp.real((near_field - u_data) * jnp.conj(near_field - u_data)))
+ 
+
+def near_field_l2_loss_fwd(params: NearFieldParams, 
+            u_data: jnp.ndarray,
+            nu_vect: jnp.ndarray) -> jnp.ndarray:
+    # forward solver to compute the misfit
+
+    Rhs = -(params.ls_params.omega**2)\
+           *nu_vect.reshape((-1,1))*params.G_sample
+
+    # defining the batched transformations
+    solver_batched = jit(vmap(jit(partial(ls_solver_batched_sigma,
+                                          params.ls_params,
+                                          nu_vect)),
+                              in_axes=1, 
+                              out_axes=1))
+
+    # solve for all the rhs in a vectorized fashion
+    Sigma = solver_batched(Rhs)
+
+    nm, n_angles = params.G_sample.shape
+
+    near_field = jnp.sum( Sigma.T.reshape((n_angles, nm, 1))\
+                         *params.G_sample.reshape((1, nm, n_angles)),
+                         axis=1)*params.hx*params.hy
+
+    res = near_field - u_data
+
+    return 0.5*jnp.sum(jnp.real(res * jnp.conj(res))), (Sigma, res, nu_vect)
+
+def near_field_l2_loss_bwd(params, u_data, fwd_res, cotangent):
+
+    # we load the density for the forward pass and the residual from the 
+    # misfit
+    Sigma, res, nu_vect = fwd_res
+
+    green_batched = jit(vmap(jit(partial(apply_green_function,
+                                         params.ls_params)),
+                             in_axes=1,
+                             out_axes=1))
+
+    # defining the batched for the adjoint problem
+    solver_adj_batched = jit(vmap(jit(partial(ls_solver_batched_sigma,
+                                          params.ls_params,
+                                          nu_vect)),
+                              in_axes=1, 
+                              out_axes=1))
+
+    conj_green_batched = jit(vmap(jit(partial(apply_conj_green_function,
+                                         params.ls_params)),
+                             in_axes=1,
+                             out_axes=1))
+
+
+
+    # computing the scattered field
+    U_s = green_batched(Sigma)
+
+    # computing the total field
+    U_total = U_s + params.G_sample
+
+    # extracting sizes to evaluate the integrals  
+    nm, n_angles = params.G_sample.shape
+
+    # assembling the adjoint RHS for the integral equation
+    Rhs_adjoint = (params.ls_params.omega**2)*jnp.conj(params.G_sample)@res
+
+    # solve for all the rhs in a vectorized fashion
+    Sigma_adj = solver_adj_batched(Rhs_adjoint)
+
+    # computing the scattered field
+    U_adj = conj_green_batched(Sigma_adj)
+
+    return (-cotangent*jnp.sum(jnp.real(U_adj*jnp.conj(U_total)), axis = 1),)
+
+
+near_field_l2_loss.defvjp(near_field_l2_loss_fwd, near_field_l2_loss_bwd)
+
+
+# @near_field_l2_loss.defjvp
+def near_field_l2_loss_grad(params, u_data, nu_vect):
+    """ Function to compute the Born approximation 
+    Lap ( u + delta u) + omega^2 (nu + delta nu) ( u + delta u) = f
+    
+    we first compute the zero-th order 
+    Lap u + omega^2 nu u = f
+    which is then used to compute the first variation
+    Lap delta u + omega^2 nu (delta u) = -omega^2 (delta nu) u
+    """
+
+    Rhs = -(params.ls_params.omega**2)\
+           *nu_vect.reshape((-1,1))*params.G_sample
+
+    # defining the batched transformations
+    solver_batched = jit(vmap(jit(partial(ls_solver_batched,
+                                          params.ls_params,
+                                          nu_vect)),
+                              in_axes=1, 
+                              out_axes=1))
+
+    green_batched = jit(vmap(jit(partial(apply_green_function,
+                                         params.ls_params)),
+                             in_axes=1,
+                             out_axes=1))
+
+    # defining the batched for the adjoint problem
+    solver_adj_batched = jit(vmap(jit(partial(ls_solver_batched_sigma,
+                                          params.ls_params,
+                                          nu_vect)),
+                              in_axes=1, 
+                              out_axes=1))
+
+    conj_green_batched = jit(vmap(jit(partial(apply_conj_green_function,
+                                         params.ls_params)),
+                             in_axes=1,
+                             out_axes=1))
+
+    # solve for all the rhs in a vectorized fashion
+    Sigma = solver_batched(Rhs)
+
+    # computing the scattered field
+    U_s = green_batched(Sigma)
+
+    # computing the total field
+    U_total = U_s + params.G_sample
+
+    # extracting sizes to evaluate the integrals  
+    nm, n_angles = params.G_sample.shape
+
+    # TODO: this is just a matrix vector multiplication
+    # it should be (params.G_sample.T)@Sigma
+    near_field = jnp.sum( Sigma.T.reshape((n_angles, nm, 1))\
+                         *params.G_sample.reshape((1, nm, n_angles)),
+                         axis=1)*params.hx*params.hy
+
+
+    # computing the residual
+    res = near_field - u_data 
+
+    # assembling the adjoint RHS for the integral equation
+    Rhs_adjoint = (params.ls_params.omega**2)*jnp.conj(params.G_sample)@res
+
+    # solve for all the rhs in a vectorized fashion
+    Sigma_adj = solver_adj_batched(Rhs_adjoint)
+
+    # computing the scattered field
+    U_adj = conj_green_batched(Sigma_adj)
+
+    return 0.5*jnp.sum(jnp.real((near_field - u_data) * jnp.conj(near_field - u_data))), -jnp.sum(jnp.real(U_adj*jnp.conj(U_total)), axis = 1)
